7abeba4402abf8850598613ef1da7a19c2e39c9a
Mikhail <misha.safronov.02.12@mail.ru>
2023-02-28 23:53:29 +0300

    Добавьте файлы проекта.

---
+++ AdBoardsMobileAndroid/AdBoardsMobileAndroid/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AdBoardsMobileAndroid.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AdBoardsMobileAndroid
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(EditingProfilePage),typeof(EditingProfilePage));
+            Routing.RegisterRoute(nameof(AdPage),typeof(AdPage));
+        }
+
+    }
+}
